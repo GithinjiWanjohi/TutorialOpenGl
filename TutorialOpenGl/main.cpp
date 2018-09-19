ee67bf90ee207b7d1a33da707968e66c4ae17eac
Githinji Wanjohi <gyth.wanjohi@gmail.com>
2018-09-19 18:58:04 +0300

    Initial Commit

---
+++ TutorialOpenGl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TutorialOpenGl
+//
+//  Created by Githinji Wanjohi on 9/19/18.
+//  Copyright © 2018 Githinji Wanjohi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
